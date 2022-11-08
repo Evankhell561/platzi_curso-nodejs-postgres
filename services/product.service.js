e078e931480e8154767522d1c0a65947fde25d8d
Evankhell561 <49106866+Evankhell561@users.noreply.github.com>
2022-11-08 19:52:11 -0400

    sequelize init

--- services/product.service.js
+++ services/product.service.js
@@ -1,14 +1,12 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
 
-  constructor(){
+  constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => ( console.error(err)));
   }
 
   generate() {
@@ -35,8 +33,9 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM task ORDER BY id ASC';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    //const [data, metadata] = await sequelize.query(query); // no necesitamos la metadata
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
